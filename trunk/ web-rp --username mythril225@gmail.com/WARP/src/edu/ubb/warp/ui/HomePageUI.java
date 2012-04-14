538a41ba60108d3f2e820dd148e2322d0de2bded
mythril225@gmail.com <mythril225@gmail.com@fa25ab9c-40a6-6f79-93f8-4ea2f5e2a82e>
2012-04-14 10:16:56 +0000

    Created BasePageUI which will be used to create all subsequent PageUI classes. Menu items not yet created, listeners not yet created. Created HomePageUI which is the very first page a user sees after logging in. Containers not yet loaded with items. Listeners not yet implemented. Visuals are completed. Modified WarpApplication to test newly created HomePageUI.

---
+++ trunk/ web-rp --username mythril225@gmail.com/WARP/src/edu/ubb/warp/ui/HomePageUI.java
@@ -0,0 +1,79 @@
+package edu.ubb.warp.ui;
+
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.event.ItemClickEvent;
+import com.vaadin.event.ItemClickEvent.ItemClickListener;
+import com.vaadin.ui.*;
+import com.vaadin.event.*;
+
+/**
+ * HomePageUI is the very first page you see after logging in Check
+ * Documentation for clarification on this application
+ * 
+ * @author Sandor
+ * 
+ */
+public class HomePageUI extends BasePageUI {
+
+	private TabSheet tabS = new TabSheet();
+	private Table projects = new Table();
+	private Table jobs = new Table();
+	private ListSelect groups = new ListSelect();
+	private HorizontalLayout hl = new HorizontalLayout();
+
+	public HomePageUI(String uType) {
+
+		super(uType);
+		this.addComponent(tabS);
+
+		/*
+		 * Setting up Tab1/Projects Tab 
+		 * Loading table w/ data 
+		 * Setting up Listener
+		 */
+		tabS.addTab(projects, "Projects");
+
+		// ---------------------------------
+		/*
+		 * Space reserved for loading table w/ data
+		 */
+		// ---------------------------------
+
+		projects.setImmediate(true);
+		projects.setSelectable(true);
+		projects.addListener(new ItemClickListener() {
+
+			public void itemClick(ItemClickEvent event) {
+
+				/*
+				 * Space reserved for handling the proper event
+				 */
+
+			}
+		});
+		
+		/*
+		 * Setting up Tab2/My Jobs Tab
+		 * Loading table w/ data 
+		 * Setting up Listener
+		 */
+		hl.addComponent(groups);
+		groups.setWidth("300px");
+		hl.addComponent(jobs);
+		tabS.addTab(hl, "Jobs");
+		
+		// ---------------------------------
+				/*
+				 * Space reserved for loading table w/ data
+				 */
+		// ---------------------------------
+		
+		//---------------------------------
+			/*
+			 * Space reserved for handling events of groups/jobs
+			 */
+		//---------------------------------
+
+	}
+
+}
