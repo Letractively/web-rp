81d0483810546ccaf83d49f07901bde4ccff22e4
szecsi.zsolt91@gmail.com <szecsi.zsolt91@gmail.com@fa25ab9c-40a6-6f79-93f8-4ea2f5e2a82e>
2012-05-16 15:23:28 +0000

    getAllStatuses metod

--- trunk/ web-rp --username mythril225@gmail.com/WARP/src/edu/ubb/warp/dao/StatusDAO.java
+++ trunk/ web-rp --username mythril225@gmail.com/WARP/src/edu/ubb/warp/dao/StatusDAO.java
@@ -1,5 +1,7 @@
 package edu.ubb.warp.dao;
 
+import java.util.ArrayList;
+
 import edu.ubb.warp.exception.DAOException;
 import edu.ubb.warp.exception.StatusNameExistsException;
 import edu.ubb.warp.exception.StatusNotFoundException;
@@ -10,26 +12,46 @@ public interface StatusDAO {
 	 * 
 	 * @param StatusID
 	 * @return Status with requested statusID
-	 * @throws DAOException in case of database access issues
-	 * @throws StatusNotFoundException in case there is no such status in the database
+	 * @throws DAOException
+	 *             in case of database access issues
+	 * @throws StatusNotFoundException
+	 *             in case there is no such status in the database
 	 */
-	Status getStatusByStatusID(int StatusID) throws DAOException, StatusNotFoundException;
+	Status getStatusByStatusID(int StatusID) throws DAOException,
+			StatusNotFoundException;
+
 	/**
 	 * 
-	 * @param Status the status to be added
-	 * @throws StatusNameExistsException in case the statusName is not unique
+	 * @param Status
+	 *            the status to be added
+	 * @throws StatusNameExistsException
+	 *             in case the statusName is not unique
 	 */
 	void insertStatus(Status status) throws StatusNameExistsException;
+
 	/**
 	 * 
-	 * @param Status the status to be added
-	 * @throws DAOException in case of database access issues
+	 * @param Status
+	 *            the status to be added
+	 * @throws DAOException
+	 *             in case of database access issues
 	 */
 	void deleteStatus(Status status) throws DAOException;
+
 	/**
 	 * 
-	 * @param Status the status to be modified
-	 * @throws StatusNameExistsException in case the statusName is not unique
+	 * @param Status
+	 *            the status to be modified
+	 * @throws StatusNameExistsException
+	 *             in case the statusName is not unique
 	 */
 	void updateStatus(Status status) throws StatusNameExistsException;
+
+	/**
+	 * 
+	 * @return all statuses in database
+	 * @throws DAOException
+	 *             in case of database access issues
+	 */
+	public ArrayList<Status> getAllStatuses() throws DAOException;
 }
