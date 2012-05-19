df387274372753d746bcd42b7f0b40c172dce2a8
szecsi.zsolt91 <szecsi.zsolt91@gmail.com>
2012-05-19 16:12:17 +0000

    RatioOutOfBoundsException with parameter

---  web-rp --username mythril225@gmail.com/WARP/src/edu/ubb/warp/exception/RatioOutOfBoundsException.java
+++  web-rp --username mythril225@gmail.com/WARP/src/edu/ubb/warp/exception/RatioOutOfBoundsException.java
@@ -2,5 +2,21 @@ package edu.ubb.warp.exception;
 
 @SuppressWarnings("serial")
 public class RatioOutOfBoundsException extends Exception {
+	private int week;	
 
+	public RatioOutOfBoundsException(){
+		
+	}
+	
+	public RatioOutOfBoundsException(int week){
+		this.week = week;
+	}
+
+	public int getWeek() {
+		return week;
+	}
+
+	public void setWeek(int week) {
+		this.week = week;
+	}
 }
