1465a4c81a13e305c31d7e59bb629b7a9d617a99
szecsi.zsolt91 <szecsi.zsolt91@gmail.com>
2012-05-21 11:46:29 +0000

    getAllProjectInTimeFrame getAllProjectByUserInTimeFrame metods updateBooking modositva

---  web-rp --username mythril225@gmail.com/WARP/src/edu/ubb/warp/dao/ProjectDAO.java
+++  web-rp --username mythril225@gmail.com/WARP/src/edu/ubb/warp/dao/ProjectDAO.java
@@ -13,8 +13,10 @@ public interface ProjectDAO {
 	 * 
 	 * @param projectID
 	 * @return Project with requested id
-	 * @throws DAOException in case of database access issues
-	 * @throws ProjectNotFoundException  in case there is no such project in the database
+	 * @throws DAOException
+	 *             in case of database access issues
+	 * @throws ProjectNotFoundException
+	 *             in case there is no such project in the database
 	 */
 	public Project getProjectByProjectID(int projectID) throws DAOException,
 			ProjectNotFoundException;
@@ -23,8 +25,10 @@ public interface ProjectDAO {
 	 * 
 	 * @param projectName
 	 * @return Project with requested name
-	 * @throws DAOException in case of database access issues
-	 * @throws ProjectNotFoundException  in case there is no such project in the database
+	 * @throws DAOException
+	 *             in case of database access issues
+	 * @throws ProjectNotFoundException
+	 *             in case there is no such project in the database
 	 */
 	public Project getProjectByProjectName(String projectName)
 			throws DAOException, ProjectNotFoundException;
@@ -32,7 +36,8 @@ public interface ProjectDAO {
 	/**
 	 * 
 	 * @param project
-	 * @throws ProjectNameExistsException in case the projectName is not unique
+	 * @throws ProjectNameExistsException
+	 *             in case the projectName is not unique
 	 */
 	public void insertProject(Project project)
 			throws ProjectNameExistsException;
@@ -40,7 +45,8 @@ public interface ProjectDAO {
 	/**
 	 * 
 	 * @param project
-	 * @throws ProjectNameExistsException in case the projectName is not unique
+	 * @throws ProjectNameExistsException
+	 *             in case the projectName is not unique
 	 */
 	public void updateProject(Project project)
 			throws ProjectNameExistsException;
@@ -48,10 +54,11 @@ public interface ProjectDAO {
 	/**
 	 * 
 	 * @param project
-	 * @throws DAOException in case of database access issues
+	 * @throws DAOException
+	 *             in case of database access issues
 	 */
 	public void deleteProject(Project project) throws DAOException;
-	
+
 	/**
 	 * 
 	 * @param user
@@ -59,4 +66,31 @@ public interface ProjectDAO {
 	 * @throws DAOException
 	 */
 	public ArrayList<Project> getProjectsByUser(User user) throws DAOException;
+
+	/**
+	 * 
+	 * @param week1
+	 *            - start week
+	 * @param week2
+	 *            - end week
+	 * @return all project in week1 week2 interval
+	 * @throws DAOException
+	 *             in case of database access issues
+	 */
+	public ArrayList<Project> getAllProjectsInTimeFrame(int week1, int week2)
+			throws DAOException;
+
+	/**
+	 * 
+	 * @param week1
+	 *            - start week
+	 * @param week2
+	 *            - end week
+	 * @param resourceID
+	 * @return all project by resourceID in week1, week2 interval
+	 * @throws DAOException
+	 *             in case of database access issues
+	 */
+	public ArrayList<Project> getAllProjectsByResourceInTimeFrame(int week1,
+			int week2, int resourceID) throws DAOException;
 }
