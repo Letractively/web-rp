6e775aedf8f2a5ce75bffdcb9a7ef44c28ca70ca
bali.sb <bali.sb@gmail.com>
2012-05-30 07:33:10 +0000

    added Colors

---  web-rp --username mythril225@gmail.com/WARP/src/edu/ubb/warp/logic/Colorizer.java
+++  web-rp --username mythril225@gmail.com/WARP/src/edu/ubb/warp/logic/Colorizer.java
@@ -1,5 +1,52 @@
 package edu.ubb.warp.logic;
 
+import java.awt.Color;
+
+/**
+ * Provides static methods for colors
+ * 
+ * @author Balazs
+ * 
+ */
 public class Colorizer {
+	public static Color color1 = new Color(220, 170, 0);
+	public static Color color2 = new Color(0, 220, 0);
+	public static Color defaultColor = new Color(220, 0, 0);
+
+	/**
+	 * 
+	 * @param input
+	 * @return the rgb hex string corresponding to the given float value
+	 */
+	public static String floatToHexString(float input) {
+		if (input == 0) {
+			return colorToHexString(defaultColor);
+		}
+		return colorToHexString(new Color(
+				limit((int) (color1.getRed() * (1 - input) + color2.getRed() * input)),
+				limit((int) (color1.getGreen() * (1 - input) + color2.getGreen() * input)),
+				limit((int) (color1.getBlue() * (1 - input) + color2.getBlue() * input))));
+	}
 	
+	/**
+	 * 
+	 * @param input
+	 * @return a div tag containing the value, with the formatting corresponding to the value
+	 */
+	public static String floatToHTML(float input) {
+		return "<div style=\"color: " + floatToHexString(input) + ";\">" + input + "</div>";
+	}
+
+	/**
+	 * 
+	 * @param color
+	 * @return the rgb hex string corresponding to the given color
+	 */
+	public static String colorToHexString(Color color) {
+		return "#" + Integer.toHexString(color.getRGB()).substring(2);
+	}
+	
+	private static int limit(int integer) {
+		return (integer < 0) ? 0 : ((integer > 255) ? 255 : integer);
+	}
 }
